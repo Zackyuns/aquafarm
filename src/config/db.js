378a93e51d04b1cb316c9734528bb35d9638fd2e
Zackyuns <muhammadzacky1324@gmail.com>
2026-07-06 08:29:57 +0700

    Initial commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,18 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
-console.log("DB NAME:", process.env.DB_NAME);
+console.log("HOST :", process.env.DB_HOST);
+console.log("PORT :", process.env.DB_PORT);
+console.log("USER :", process.env.DB_USER);
+console.log("DB   :", process.env.DB_NAME);
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 4000),
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
@@ -16,8 +21,6 @@ const pool = mysql.createPool({
         minVersion: "TLSv1.2",
         rejectUnauthorized: true
     }
-
 });
 
-// Menggunakan promise wrapper agar bisa pakai async/await
 module.exports = pool.promise();
